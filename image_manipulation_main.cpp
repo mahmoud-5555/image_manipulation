ebfae0931912220da23567f0117b1a0c1bb2c27c
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-10-06 16:45:50 +0300

    update main

--- image_manipulation_main.cpp
+++ image_manipulation_main.cpp
@@ -11,94 +11,252 @@ void loadImage ();
 void saveImage ();
 void doSomethingForImage ();
 
+
 int main()
 {
 char ordar;
+char exit_test;
   //welcome menu 
   cout << "welcome to image editor \n";
-  
-  cout << "Please enter file name of the image to process: \n";
-  loadImage();
-  cout <<"1-	Black & White Filter\n 2-	Invert Filter\n 3- Merge Filter\n";
-  cout << "4-	Flip Image\n 5-	Darken and Lighten Image\n 6-	Rotate Image\n";
-  cout << "7-	Detect Image Edges 8-	Enlarge Image 9-	Shrink Image\n";
-  cout << "a-	Mirror 1/2 Image\n b-	Shuffle Image\n c-	Blur Image\n d-	Crop Image\n";
-  cout <<  "e-	Skew Image Right\n f-	Skew Image Up \n Save the image to a file ";
-  cout <<"0-	Exit\n z- do anther change in same file\n r- reloadfile (new file) ";
-  cin >> ordar;
-  switch ((int) ordar)
+  while (true)
   {
-  case '1' /*Black & White Filter*/:
-    /* code */
-    break;
+    cout << "Please enter file name of the image to process: \n";
 
-    case '2' /*Invert Filter*/:
-    /* code */
-    break;
+    cout <<"1-	Black & White Filter\n 2-	Invert Filter\n 3- Merge Filter\n";
+    cout << "4-	Flip Image\n 5-	Darken and Lighten Image\n 6-	Rotate Image\n";
+    cout << "7-	Detect Image Edges 8-	Enlarge Image 9-	Shrink Image\n";
+    cout << "a-	Mirror 1/2 Image\n b-	Shuffle Image\n c-	Blur Image\n d-	Crop Image\n";
+    cout <<  "e-	Skew Image Right\n f-	Skew Image Up \n Save the image to a file ";
+    cout <<"0-	Exit\n z- do anther change in same file\n l- loadfile (new file) ";
+    cin >> ordar;
+    switch ((int) ordar)
+    {
+      case '1' /*Black & White Filter*/:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case '3' /*Merge Filter */:
-    /* code */
-    break;
+      case '2' /*Invert Filter*/:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case '4' /*Flip Image*/:
-    /* code */
-    break;
+      case '3' /*Merge Filter */:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case '5' /*Darken and Lighten Image */:
-    /* code */
-    break;
+      case '4' /*Flip Image*/:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case '6' /*Rotate Image*/:
-    /* code */
-    break;
+      case '5' /*Darken and Lighten Image */:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case '7' /*Detect Image Edges */:
-    /* code */
-    break;
+      case '6' /*Rotate Image*/:
+      if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case '8' /*Enlarge Image */:
-    /* code */
-    break;
+      case '7' /*Detect Image Edges */:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case '9' /*Shrink Image*/:
-    /* code */
-    break;
+      case '8' /*Enlarge Image */:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case 'a' /*Mirror 1/2 Image*/:
-    /* code */
-    break;
+      case '9' /*Shrink Image*/:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case 'b' /*Shuffle Image*/:
-    /* code */
-    break;
+      case 'a' /*Mirror 1/2 Image*/:
+      if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case 'c' /*Blur Image*/:
-    /* code */
-    break;
+      case 'b' /*Shuffle Image*/:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case 'd' /*Crop Image*/:
-    /* code */
-    break;
+      case 'c' /*Blur Image*/:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case 'e' /*Skew Image Right*/:
-    /* code */
-    break;
+      case 'd' /*Crop Image*/:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
-    case 'f' /*Skew Image Up*/:
-    /* code */
-    break;
+      case 'e' /*Skew Image Right*/:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
+
+      case 'f' /*Skew Image Up*/:
+        if (load)
+        {
+          /* code */
+        }
+        else
+        {
+          cout <<"pleae load the file first befor using the tool\n";
+        }
+        break;
 
     case 's' /*Save the image to a file*/:
-    /* code */
+    if (change)
+    {
+      saveImage();
+      cout <<"file saved :: you can load anther file or complete edit in same file \n ";
+      change = false;
+      seved = true;
+    }
+    else
+      cout << "no file change plese make change first \n ";
     break;
 
-    case 'r' /*reloadfile (new file)*/:
-    /* code */
+    case 'l' /*loadfile (new file)*/:
+    loadImage();
+    load = true;
+    change = false;
     break;
+
+    case '0' /*0-	Exit*/:
+
+    if(seved)
+    {
+      cout << "exit done \n";
+        return (0);
+    }
+    else{
+      bool cancel = false;
+      while (!cancel)
+      {
+        cout <<"this file has change didn't saved do you sure that you wnna exit ?\n     s: save first    y: exit     n: cancel \n"; 
+        cin >> exit_test;
+        switch (exit_test)
+        {
+        case 's' /* Save the image to a file */:
+          saveImage();
+          cout << "saved \n";
+          return (0);
+          break;
+
+        case 'y' /* exit with out saving */:
+          cout <<"exit sucssifull";
+            return (0);
+          break;
+
+        case 'n' /* constant-expression */:
+          cancel = true;
+          break;
+        
+        default:
+        cout << exit_test << "  : is not valid input \n";
+          break;
+        }
+      } 
+    }
+        break;
   
-  default:
-    break;
+    default:
+        cout << ordar << "  : is not valid input \n";
+      break;
   }
+}  
 
 
 
@@ -106,7 +264,7 @@ char ordar;
 
   //
   //doSomethingForImage();
-  //saveImage();
+  //
   return 0;
 }
 
@@ -117,6 +275,7 @@ void loadImage () {
    // Get gray scale image file name
    cout << "Enter the source image file name: ";
    cin >> imageFileName;
+   cout << '\n';
 
    // Add to it .bmp extension and load image
    strcat (imageFileName, ".bmp");
@@ -130,6 +289,7 @@ void saveImage () {
    // Get gray scale image target file name
    cout << "Enter the target image file name: ";
    cin >> imageFileName;
+   cout << '\n';
 
    // Add to it .bmp extension and load image
    strcat (imageFileName, ".bmp");
--- main.h
+++ main.h
@@ -3,6 +3,10 @@
 #include "bmplib.h"
 
 unsigned char image[SIZE][SIZE];
+bool load = false;
+bool change = false;
+bool seved = false;
+void Invert(void);
 /* The declareing of all function*/
 
 #endif
