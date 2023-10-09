a7272466930e2c34f3ac27cfade7fcd8ab753a65
Elniel <corgoo7z49h@gmail.com>
2023-10-10 01:12:01 +0300

    Rotate Image v2

--- Rotate_Image.cpp
+++ Rotate_Image.cpp
@@ -1,24 +1,20 @@
-#include<iostream>
-#include "main.h"
-using namespace std;
 /**
  * RotateImage - The function rotate image the function ask the user to enter degree between : (90) or (180) or (360) 
  * if the user enter:
  *  90 : it will reverse the columns of the matrix 
  *  180 : it will reverse rows and columns of matrix
- *  360 : it not do nothing
+ *  270 : transpose the matrix then reverse rows
  * 
  * 
  * Return : no return value
- * 
-*/
+ **/
 
 // 6 - rotate image
-void rotateImage(void) {
+void rotateImage() {
 
   int degree;
-  // ask user to Enter the degree
-  cout << "Rotate (90), (180) or (360) degrees?";
+  // ask user to Enter rotate degree
+  cout << "Rotate (90), (180) or (270) degrees?";
   cin>> degree;
   unsigned char rotated_image[SIZE][SIZE];
   if (degree == 90) {
@@ -66,5 +62,21 @@ void rotateImage(void) {
 
   }
 
+  else if (degree == 270) {
+        // Transpose the matrix
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = i + 1; j < SIZE; j++) {
+            swap(image[i][j], image[j][i]);
+        }
+    }
+
+    // Reverse the rows
+    for (int i = 0; i < SIZE / 2; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            swap(image[i][j], image[SIZE - 1 - i][j]);
+        }
+    }
+  }
 
 }
+
