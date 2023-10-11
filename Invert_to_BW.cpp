0dcfd098705b8e97b170945ccf102aa224991e3d
mahmoud-5555 <mahmoudadam5555@gmail.com>
2023-10-11 10:45:28 +0300

    last commit

--- Invert_to_BW.cpp
+++ Invert_to_BW.cpp
@@ -1,7 +1,4 @@
-#include<iostream>
 #include "main.h"
-#include "bmplib.h"
-using namespace std;
 
 /*
 *InvertBW: invert   image to black and white
@@ -13,7 +10,6 @@ void InvertBW(void)
 {
  for(int i = 0; i < SIZE;i++){
  	for(int j = 0; j < SIZE; j++){
- 		
  		
  		if(image[i][j] < 127){
  			
