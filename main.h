5a835aa37a652eb945037aad26f77be369e13406
unknown <hassanabdulgader16@gmail.com>
2023-10-19 11:26:49 -0700

    This main.h is for Image_manipulation  project

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<iostream>
+#include "bmplib.h"
+#include <cstring>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+extern unsigned char image[256][256];
+
+
+void blackWhiteFilter();
+void blurImage();
+void cropImage();
+int darkenLighten();
+void enlargeImage();
+int flip_image();
+void Invert();
+void mergeFilter();
+void mirrorImage();
+void rotateImage();
+void skewImageRight();
+void skewImageUp();
+void shrinkImage();
+void shuffleImage();
+void DetectImageEdges();
+
+#endif
